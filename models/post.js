037d5233c10ed7b958cf6e74b278e216cd837d7f
xiao ching <carol.xiao.ching@gmail.com>
2025-05-02 00:45:28 +0800

    [feat] 建立 Post Schema

--- models/post.js
+++ models/post.js
@@ -2,9 +2,59 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
   {
+    // 發文者
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+      required: [true, "請提供發文者 ID"],
+    },
+    // 貼文內容
     content: {
       type: String,
-      required: [true, "貼文內容 未填寫"],
+      required: [true, "請輸入貼文內容"],
+      maxlength: [300, "貼文內容不得超過 300 字"],
+    },
+    // 圖片
+    images: {
+      type: [
+        {
+          imageUrl: { type: String, required: true },
+          imageId: {
+            type: mongoose.Schema.ObjectId,
+            required: true,
+            ref: "image",
+          },
+        },
+      ],
+      validate: {
+        validator(value) {
+          return value.length <= 2;
+        },
+        message: "最多只能上傳 2 張圖片",
+      },
+      default: [],
+    },
+    // 按讚的人
+    likes: {
+      type: [
+        {
+          type: mongoose.Schema.ObjectId,
+          ref: "user",
+        },
+      ],
+      default: [],
+    },
+    // 評論數量
+    commentsCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    // 按讚數量
+    likesCount: {
+      type: Number,
+      min: 0,
+      default: 0,
     },
   },
   {
