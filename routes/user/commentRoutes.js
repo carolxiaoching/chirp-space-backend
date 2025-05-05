c7a6a0fc923a23440123933970c6393eddf19a40
xiao ching <carol.xiao.ching@gmail.com>
2025-05-05 20:28:06 +0800

    [refactor] 拆分 comments controllers 結構

--- routes/user/commentRoutes.js
+++ routes/user/commentRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const CommentControllers = require("../../controllers/user/comments");
+const comment = require("../../controllers/comments");
 const errorAsyncHandler = require("../../services/errorAsyncHandler");
 const {
   checkTokenAndSetAuth,
   getUserFromAuthId,
 } = require("../../middleware/authMiddleware");
 
+// commentBase
+
 // 刪除評論
 router.delete(
   "/comment/:commentId",
   checkTokenAndSetAuth,
   getUserFromAuthId,
-  errorAsyncHandler(CommentControllers.deleteComment)
+  errorAsyncHandler(comment.deleteComment)
 );
 
 module.exports = router;
